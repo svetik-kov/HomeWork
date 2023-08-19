45a3c5c8e35124ef1013a9b829b91b55de73dcb4
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-08-19 22:13:05 +0300

    HW_8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,14 +7,15 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let copyState=[...state]
             if (action.payload === 'up') {
 
-                return state.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1)
+                return  copyState.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1)
             }
             if (action.payload === 'down') {
-                return state.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
+                return  copyState.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
             }
-            return state // need to fix
+            return  copyState // need to fix
         }
         case 'check': {
 
