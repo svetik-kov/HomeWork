132afe52f599eac14c8c670395a5cab1f645603f
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-10-18 22:17:50 +0300

    HW_11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,11 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                borderRadius: '10px',
+                color: '#00CC22',
+                width: '147px',
+                height: '4px',
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
